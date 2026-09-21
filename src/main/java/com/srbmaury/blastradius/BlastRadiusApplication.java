3b00fb38f9d287dffd99578775b3387c43175891
Saurabh Maurya <srbmaury@gmail.com>
2026-09-21 15:39:12 +0530

    feat: add Spring Boot entrypoint

---
+++ src/main/java/com/srbmaury/blastradius/BlastRadiusApplication.java
@@ -0,0 +1,12 @@
+package com.srbmaury.blastradius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlastRadiusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlastRadiusApplication.class, args);
+    }
+}
